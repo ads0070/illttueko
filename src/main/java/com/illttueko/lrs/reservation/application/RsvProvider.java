4b9a10d7111c6ede1bb743cd41a64b8739987a5a
ads0070 <gch05610@naver.com>
2022-05-31 05:54:01 +0900

    공지 조회 및 수정 API 추가

--- src/main/java/com/illttueko/lrs/reservation/application/RsvProvider.java
+++ src/main/java/com/illttueko/lrs/reservation/application/RsvProvider.java
@@ -1,9 +1,12 @@
 package com.illttueko.lrs.reservation.application;
 
+import com.illttueko.config.BaseException;
 import com.illttueko.lrs.reservation.domain.NoticeDto;
 import com.illttueko.lrs.reservation.infrastructure.NoticeJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import static com.illttueko.config.BaseResponseStatus.*;
 
 @Service
 public class RsvProvider {
@@ -15,8 +18,16 @@ public class RsvProvider {
         this.noticeJpaRepository = noticeJpaRepository;
     }
 
-    public NoticeDto retrieveNotice() {
-        return noticeJpaRepository.findByIdx(1);
+    public NoticeDto retrieveNotice() throws BaseException {
+        try {
+            NoticeDto noticeDto = noticeJpaRepository.findByIdx(1);
+            if(noticeDto == null) {
+                throw new BaseException(DATABASE_ERROR);
+            }
+            return noticeDto;
+        } catch (Exception exception) {
+            throw new BaseException(DATABASE_ERROR);
+        }
     }
 
 }
--- src/main/java/com/illttueko/lrs/reservation/application/RsvService.java
+++ src/main/java/com/illttueko/lrs/reservation/application/RsvService.java
@@ -1,24 +1,27 @@
 package com.illttueko.lrs.reservation.application;
 
+import com.illttueko.config.BaseException;
 import com.illttueko.lrs.reservation.domain.Notice;
 import com.illttueko.lrs.reservation.domain.NoticeDto;
 import com.illttueko.lrs.reservation.infrastructure.NoticeJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import static com.illttueko.config.BaseResponseStatus.*;
+
 @Service
 public class RsvService {
-    @Autowired
+
     private final NoticeJpaRepository noticeJpaRepository;
-    @Autowired
     private final RsvProvider rsvProvider;
 
+    @Autowired
     public RsvService(NoticeJpaRepository noticeJpaRepository, RsvProvider rsvProvider) {
         this.noticeJpaRepository = noticeJpaRepository;
         this.rsvProvider = rsvProvider;
     }
 
-    public void updateNotice(NoticeDto noticeDto) throws Exception {
+    public void updateNotice(NoticeDto noticeDto) throws BaseException {
         NoticeDto originNotice = rsvProvider.retrieveNotice();
 
         if(noticeDto.getNotice() != null) {
@@ -29,12 +32,11 @@ public class RsvService {
         }
 
         Notice newNotice = originNotice.toEntity();
-        noticeJpaRepository.save(newNotice);
 
         try {
             noticeJpaRepository.save(newNotice);
         } catch (Exception exception) {
-            throw new Exception(exception);
+            throw new BaseException(DATABASE_ERROR);
         }
     }
 
