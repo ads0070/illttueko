71053954131f80198adf6e76e3c9341de201fe89
ads0070 <gch05610@naver.com>
2022-05-31 05:53:08 +0900

    공지 조회 및 수정 HTTP 요청

---
+++ src/main/resources/static/js/notice.js
@@ -0,0 +1,50 @@
+$(document).ready(function($) {
+    $("#notice-patch").click(function () {
+        var getNotice = $("#notice").val();
+        $.ajax({
+            url: "/reservation/notice",
+            type: "patch",
+            contentType : 'application/json; charset=UTF-8',
+            data: JSON.stringify({
+                notice: getNotice,
+                name : name
+            }),
+            datatype: "json",
+            success: function (data) {
+                console.log(data);
+                setAlert(data);
+            },
+            error: function () {
+                //
+            }
+        })
+    })
+
+    $("#rule-patch").click(function () {
+        var getRule = $("#rule").val();
+        $.ajax({
+            url: "/reservation/notice",
+            type: "patch",
+            contentType : 'application/json; charset=UTF-8',
+            data: JSON.stringify({
+                rule: getRule
+            }),
+            datatype: "json",
+            success: function (data) {
+                console.log(data);
+                setAlert(data);
+            },
+            error: function () {
+                //
+            }
+        })
+    })
+
+    function setAlert(data) {
+        if(data.code === 1000) {
+            alert(data.result);
+        } else{
+            alert(data.message);
+        }
+    }
+});
